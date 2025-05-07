da76cb3ba2ec1f896484399d43f25cc61ad75e72
Josué <josueeliashuaman@gmail.com>
2025-05-06 19:16:37 -0500

    feat: add detailed analysis and prompts for Urbania's business model and financial flows

---
+++ prompts.py
@@ -0,0 +1,34 @@
+
+empresa = 'urganibia'
+prompt1 = f'''ok quiero que me des flujo que explica la forma en que {empresa} gana dinero. se lo mas explicito posible. el resultado tiene que ser en mermaid.'''
+
+
+
+'''
+REQUERIMIENTO
+Requiero un análisis detallado de Urbania, una empresa que se dedica al sector inmobiliarias. El análisis debe incluir:
+- el modelo de negocio de Urbania.
+- quiero saber como se relaciona con sus clientes, proveedores y competidores.
+- quiero saber si esta perdiiendo dinero por un lado para ganar market share.
+- al final debes de darme todas las referencias y fuentes de donde sacaste la información. Estas deben estar listadas y con links a las páginas de donde sacaste la información.
+- también tienes que darme un resumen de 5 puntos de lo que me diste. 
+- IMPORTANTE: buscar en internet información actualizada de Urbania, incluyendo su página web oficial y otras fuentes confiables.
+
+DIAGRAMA DE FLUJOS
+- incluye un diagrama de pie donde se detallen los ingresos y egresos de Urbania anuales
+- incluye un diagrama de flujo donde se detalle el marketshare en base a sus principales competidores.
+- detecta cuales son los actores necesarios para el funcionamiento de Urbania. En base a ello has un diagrama de flujo de cada uno donde se detalle los pasos que sigue cada uno de ellos en la empresa.
+- flujo de ingresos: detalla como es que Urbania gana dinero, incluyendo los ingresos por comisiones, publicidad y otros ingresos.
+- flujo de egresos
+
+SOBRE EL CONTENIDO
+- el texto debe ser lo suficientemente largo como para que el lector entienda el tema.
+- Ten la libertad de explciar lo que consideres necesario para que el lector entienda el tema.
+- el texto debe ser claro y conciso ya que quien leera el documento no es una persona tecnica.
+- el texto debe estar en español y en un tono formal
+
+FORMATO
+- para el caso de los diagramas mermaid, dentro de los corchetes "[]" no pongas parentesis "()" porque no es reconocido.
+- el texto debe estar listo para copiar y pegar en un documento de markdown
+- de ser necesario, incluir tablas, gráficos en mermaid, diagramas de flujo, diagramas de gantt, etc 
+'''
