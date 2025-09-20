53439b51ecb9dc71bf36fd89d25e57a05e675907
Josue <josueeliashuaman@gmail.com>
2025-09-20 20:28:20 +0000

    BREAKING CHANGE: finish startup project

--- frontend/nextjs-dashboard/app/ui/acme-logo.tsx
+++ frontend/nextjs-dashboard/app/ui/acme-logo.tsx
@@ -5,7 +5,7 @@ export default function AcmeLogo() {
     return (
         <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
             <GlobeAltIcon className='h-12 w-12 rotate-[15deg]' />
-            <p className='text-[44px]'>Acme</p>
+            <p className='text-[44px] '>Acme</p>
         </div>
     )
 }
--- frontend/nextjs-dashboard/app/ui/invoices/status.tsx
+++ frontend/nextjs-dashboard/app/ui/invoices/status.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx'
 export default function InvoiceStatus({ status }: { status: string }) {
     return (
         <span
-            className={clsx('inline-flex items-center rounded-full px-2 py-1 text-sm', {
+            className={clsx('inline-flex items-center rounded-full px-2 py-1 text-xs', {
                 'bg-gray-100 text-gray-500': status === 'pending',
                 'bg-green-500 text-white': status === 'paid'
             })}
